f5f83ba063593cebe3c1387a1cec1c6030a827cf
khusanmv <105859045+Khusanmv@users.noreply.github.com>
2022-12-25 21:17:59 +0500

    Add files via upload

---
+++ Lesson 19/index.js
@@ -0,0 +1,58 @@
+// function img()
+// {
+
+//     let img = document.getElementsByClassName('img1')
+//     {
+//         for (let i = 0; i < img.length; i++) {
+
+//             img[i].style.borderRadius = "20px"
+     
+            
+//         }
+//     }
+// }
+
+// function im()
+// {
+//     let imgg = document.getElementsByClassName('img2')
+//     {
+//         for (let j = 0; j < imgg.length; j++) {
+            
+//             imgg[j].style.borderRadius = '50%';
+            
+//         }
+//     }
+// }
+document.body.style.background = "antiquewhite"
+function teg()
+{
+
+    let text = document.getElementsByTagName('h3')
+    {
+        for (let i = 0; i < text.length; i++) {
+            
+            text[0].style.color = "red";
+            text[2].style.color = "green";
+            text[i].style.textShadow = '0 0 25px red';
+        }
+
+        
+    }
+}
+
+function te1()
+{
+    let text1 = document.getElementsByTagName('p')
+    {
+
+        for (let j = 0; j < text1.length; j++) {
+
+            text1[j].style.color = 'gray';
+            text1[j].style.textAlign = 'center';
+            text1[j].style.textShadow = "0 0 15px blue"
+
+           
+            
+        }
+    }
+}
